bda603a2a3fb1d1f0fdc2c234bf123318f69de8c
Lou-John <loujohn.genthon@gmail.com>
2023-04-11 23:05:03 +0100

    new posts + mongoose

--- nodejs-blog-master/controllers/homePage.js
+++ nodejs-blog-master/controllers/homePage.js
@@ -1,6 +1,6 @@
 const Post = require("../database/models/Post");
 module.exports = async (req, res) => {
-    const posts = await Post.find({}).sort( { "_id": -1 } ).limit(3);
+    const posts = await Post.find({}).sort( { "_id": -1 } ).limit(10);
     res.render("index", {
         posts,
     });
